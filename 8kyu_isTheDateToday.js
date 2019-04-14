2987cae6700e12f1a349b64e392e20d81f82c92e
sandrine <sandrinezocca@gmail.com>
2019-04-14 16:06:52 +0200

    correction error file

--- 8kyu_isTheDateToday.js
+++ 8kyu_isTheDateToday.js
@@ -1,35 +1,27 @@
 /**
- * Un isogramme est un mot qui n'a pas de lettres répétitives, consécutives ou non. Implémentez une fonction qui détermine si une 
- * chaîne contenant uniquement des lettres est un isogramme. Supposons que la chaîne vide est un isogramme. Ignorer la casse des 
- * lettres.
- * 
- * isIsogram( "Dermatoglyphics" ) == true
- * isIsogram( "aba" ) == false // -- les mêmes caractères ne peuvent pas être adjacents
- * isIsogram("moOse") == false // -- les mêmes caractères peuvent ne pas être identiques
- * isIsogram("") == true // -- une chaîne vide est un isogramme valide
+  Ecrivez une fonction simple qui prend en paramètre un objet de date et renvoie une valeur booléenne indiquant si la date est aujourd’hui ou non.
+
+    Assurez-vous que votre fonction ne renvoie pas de faux positif en vérifiant simplement le jour.
  */
 
-function isIsogram(str){
-    //divise la chaîne en un tableau de chaînes et retourne la chaîne de caractères courante en majuscule pour ignorer la casse
-    var arr = str.split('').map(function(el) {
-      return el.toUpperCase();
-    }); 
-    console.log(arr);
-    
-    //recherhe des lettres doublons
-    return arr.every(function(el) {
-      return arr.indexOf(el) == arr.lastIndexOf(el);
-    });
+function isToday(date) {
+    let today = new Date();
+    if (date.toDateString() == today.toDateString()) {
+        return(true);    
+    }else{
+        return(false);   
+    }
   }
-  
-  /*
-  * **********
-  * test
-  * **********
-  */
-  //console.log(isIsogram("Dermatoglyphics")); // true --> OK
-  //console.log(isIsogram("isogram")); // true --> OK
-  //console.log(isIsogram("aba")); // false --> OK
-  //console.log(isIsogram("moOse")); // false --> OK
-  //console.log(isIsogram("isIsogram")); // false  -->OK
-  //console.log(isIsogram("")); // true --> OK
+
+var tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+var yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+
+
+/**
+isToday(new Date()), true;
+isToday(tomorrow), false;
+isToday(yesterday), false;
+ */
